adeff8569fc15ce4f1a673c5b4e1326950c3c83f
David Rodriguez Alonso <d.rodriguezalon@alumnos.urjc.es>
2019-04-17 18:52:58 +0200

    Ahora js02.js acepta velocidades tanto como string como numero

--- practica03/js02.js
+++ practica03/js02.js
@@ -5,44 +5,51 @@ let units, unit;
 units = Object.keys(p);
 
 function converter(v, u) {
-    if (v !== undefined && typeof v === 'number' && u !== undefined) {
+    let old_v = v;
+    v = parseInt(v);
+    if (v !== undefined && !isNaN(v) && typeof v === 'number' && u !== undefined) {
         for(unit of units) {
             if (u === unit) {
                 return v * p[unit];
             }
         }
-        console.log("Error: can't convert to unit ", u);
+        console.log("Error: can't convert to unit", u);
         return;
-    }else if (v === undefined) {
-        console.log("Error: invalid params velocity: ", v);
+    }else if (v === undefined || isNaN(v)) {
+        console.log("Error: invalid params velocity:", old_v);
         return;
     }else if (u === undefined) {
-        console.log("Error: invalid params unit: ", u);
+        console.log("Error: invalid params unit:", u);
         return;
     }else if (v !== 'number') {
-        console.log("Error: invalid params unit (not a number): ", v);
+        console.log("Error: invalid params unit (not a number):", v);
         return;
     }
 
     return;
 }
-// convertion to m/s
-console.log(converter(1, "m/s"));
-console.log(converter(50, "m/s"));
 
-// convertion to km/h
-console.log(converter(1, "km/h"));
-console.log(converter(50, "km/h"));
+function main() {
+    // convertion to m/s
+    console.log(converter(1, "m/s"));
+    console.log(converter(50, "m/s"));
 
-// convertion to mph
-console.log(converter(1, "mph"));
-console.log(converter(50, "mph"));
+    // convertion to km/h
+    console.log(converter(1, "km/h"));
+    console.log(converter(50, "km/h"));
 
-// bad convertion: velocity is not a number
-console.log(converter("patata", "mph"));
+    // convertion to mph
+    console.log(converter(1, "mph"));
+    console.log(converter(50, "mph"));
 
-//bad convertion: invalid number of params
-console.log(converter(1));
+    // bad convertion: velocity is not a number
+    console.log(converter("patata", "mph"));
 
-//bad convertion: not found unit "patata"
-console.log(converter(1, "patata"));
+    //bad convertion: invalid number of params
+    console.log(converter(1));
+
+    //bad convertion: not found unit "patata"
+    console.log(converter(1, "patata"));
+}
+
+main();
